336d764faf7cdaf53e2c6212a1c9b67b3baa98c4
pratikmahulkar <34033140+pratikmahulkar@users.noreply.github.com>
2017-11-27 20:48:20 +0530

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,31 @@
+﻿//var csv = require('node-csv').createParser();
+var csvtojson = require('csvtojson');
+//var csv_str = "1,2,3\n4,5,6";
+
+//csv.parse(csv_str, function (err, data) {
+
+//    console.log(data); //Outputs: [[1,2,3],[4,5,6]]
+
+//});
+
+// or
+
+//csv.parseFile('14NOV2017.csv', function (err, data) {
+//    data.forEach(function (row, index) {
+//        if (index == 0) {
+//        } else {
+//        }
+//        console.log(row + "-" + index);
+//    });
+//});
+
+var csvFilePath = '14NOV2017.csv';
+
+csvtojson()
+.fromFile(csvFilePath)
+.on('json', function (data) {
+    console.log(data);
+})
+.on('done', function () {
+    console.log('end')
+});
