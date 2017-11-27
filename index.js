95591d106fd2e3ca249f7a6592876dff208ed0d9
pratikmahulkar <34033140+pratikmahulkar@users.noreply.github.com>
2017-11-27 22:09:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-﻿var express = require('express')
-var app = express()
+﻿var express = require('express');
+var app = express();
+app.listen(process.env.PORT || 5000, function () { console.log('Example app listening on port 3000!') });
 
-app.set('port', (process.env.PORT || 5000))
-//app.use(express.static(__dirname + '/public'))
-
-app.get('/', function (request, response) {
-    response.send('Hello World!')
-})
-
-app.listen(app.get('port'), function () {
-    console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get('/', function (req, res) {
+    res.send('hello' + req); // load the single view file (angular will handle the page changes on the front-end)
+});
